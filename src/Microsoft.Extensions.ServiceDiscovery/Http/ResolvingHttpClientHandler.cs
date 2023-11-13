06f2f7beb0931ab6b60db0f0e476cbf231dd91ec
Reuben Bond <reuben.bond@gmail.com>
2023-11-13 13:45:09 -0800

    Service Discovery refactoring

--- src/Microsoft.Extensions.ServiceDiscovery/Http/ResolvingHttpClientHandler.cs
+++ src/Microsoft.Extensions.ServiceDiscovery/Http/ResolvingHttpClientHandler.cs
@@ -1,7 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.Extensions.Internal;
 using Microsoft.Extensions.ServiceDiscovery.Abstractions;
 
 namespace Microsoft.Extensions.ServiceDiscovery.Http;
@@ -9,9 +8,16 @@ namespace Microsoft.Extensions.ServiceDiscovery.Http;
 /// <summary>
 /// <see cref="HttpClientHandler"/> which resolves endpoints using service discovery.
 /// </summary>
-public class ResolvingHttpClientHandler(HttpServiceEndPointResolver resolver) : HttpClientHandler
+public class ResolvingHttpClientHandler : HttpClientHandler
 {
-    private readonly HttpServiceEndPointResolver _resolver = resolver;
+    private readonly HttpServiceEndPointResolver _resolver;
+    private readonly TimeProvider _timeProvider;
+
+    internal ResolvingHttpClientHandler(HttpServiceEndPointResolver resolver, TimeProvider timeProvider)
+    {
+        _resolver = resolver;
+        _timeProvider = timeProvider;
+    }
 
     /// <inheritdoc/>
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -19,7 +25,7 @@ public class ResolvingHttpClientHandler(HttpServiceEndPointResolver resolver) : 
         var originalUri = request.RequestUri;
         IEndPointHealthFeature? epHealth = null;
         Exception? error = null;
-        var responseDuration = ValueStopwatch.StartNew();
+        var startTimestamp = _timeProvider.GetTimestamp();
         if (originalUri?.Host is not null)
         {
             var result = await _resolver.GetEndpointAsync(request, cancellationToken).ConfigureAwait(false);
@@ -39,7 +45,8 @@ public class ResolvingHttpClientHandler(HttpServiceEndPointResolver resolver) : 
         }
         finally
         {
-            epHealth?.ReportHealth(responseDuration.GetElapsedTime(), error); // Report health so that the resolver pipeline can take health and performance into consideration, possibly triggering a circuit breaker?.
+            var elapsed = _timeProvider.GetElapsedTime(startTimestamp);
+            epHealth?.ReportHealth(elapsed, error); // Report health so that the resolver pipeline can take health and performance into consideration, possibly triggering a circuit breaker?.
             request.RequestUri = originalUri;
         }
     }
