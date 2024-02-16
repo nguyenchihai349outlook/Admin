6c489e3c3fc77b4c6d6fafb59f4ba00b8189a09a
David Fowler <davidfowl@gmail.com>
2024-02-15 19:09:31 -0800

    Use bicep cosmos

--- playground/CosmosEndToEnd/CosmosEndToEnd.AppHost/Program.cs
+++ playground/CosmosEndToEnd/CosmosEndToEnd.AppHost/Program.cs
@@ -1,9 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Aspire.Hosting.Azure;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-var db = builder.AddAzureCosmosDB("cosmos")
+var db = builder.AddBicepCosmosDb("cosmos")
                 .UseEmulator()
                 .AddDatabase("db");
 
